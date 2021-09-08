927dac555054ff22e7226f9d2c26c457ff47c1c2
lruizcoines <lruizcoines@gmail.com>
2021-09-08 18:32:24 -0300

    api y componentes

--- src/components/ContainerPeliculas/ContainerPeliculas.js
+++ src/components/ContainerPeliculas/ContainerPeliculas.js
@@ -8,11 +8,19 @@ export default class ContainerPeliculas extends Component {
     }
 
     componentDidMount() {
-        fetch('')
+        fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=744aeafc438e3c43559433a4ec05ce25&language=en-US&page=1')
             .then(response => { return response.json() })
             .then(data => {
                 console.log(data)
             })
             .catch(error => console.log(error));
+    }
+
+    render() {
+        return (
+            <div className = 'container'>
+                Hola
+            </div>
+        )
     }
 }
